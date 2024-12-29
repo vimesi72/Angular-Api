5195a5c841f08f0455ab9d5fc8b5e092bb489579
vimesi <116082176+vimesi72@users.noreply.github.com>
2024-12-29 09:15:39 -0500

    firs AngularApi

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent implements OnInit {
+  data: any = {};
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.llenarData();
+  }
+
+  llenarData() {
+    this.apiService.getdata().subscribe((data) => {
+      this.data = data;
+      console.log(this.data);
+    });
+  }
+}
